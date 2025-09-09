63d49ce4d822d36ed1bbf0c1565850c95cb307b2
oscarlr16 <oscarleon.ro16@gmail.com>
2025-09-08 22:54:55 -0600

    refactor: Update MQTT client and fix logging bug

--- access_server/mqtt_client.py
+++ access_server/mqtt_client.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+from paho.mqtt.client import CallbackAPIVersion
 import os
 from flask import current_app
 from dotenv import load_dotenv
@@ -14,15 +15,15 @@ TOPIC_GATE_CMD = "gate/cmd"
 
 
 def start_mqtt(app, db, User, log):
-    client = mqtt.Client()
+    client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
 
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
+    def on_connect(client, userdata, flags, reason_code, properties):
+        if reason_code == 0:
             print("Successfully connected to the MQTT broker")
             client.subscribe(TOPIC_REQUESTS)
             print(f"Subscribed to the topic: {TOPIC_REQUESTS}")
         else:
-            print(f"MQTT conection error (code: {rc})")
+            print(f"MQTT connection error (code: {reason_code})")
 
     client.on_connect = on_connect
 
@@ -36,7 +37,9 @@ def start_mqtt(app, db, User, log):
             decision = "ALLOW" if allow else "DENY"
 
             # log
-            db.sesion.add(Log(code=code, decision=decision, msg="from_camera"))
+            db.session.add(
+                log(code=code, decision=decision, msg="from_camera")
+            )  # Corrected db.sesion.add(Log()) to db.session.add(log())
             db.session.commit()
 
             # action on the barrier
